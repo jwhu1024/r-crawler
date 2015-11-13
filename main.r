af8014053c809ba568beab652de7c912f185e9f0
lester <jwhu1024@gmail.com>
2015-11-13 17:39:27 +0800

    sample output

--- main.r
+++ main.r
@@ -1,3 +1,4 @@
+library("stringr")
 source("utils.r")
 source("crawler.r")
 
@@ -13,7 +14,7 @@ get_type_m_content <- 2
 subdir <- "data"
 
 # initial before running
-basedir <- paste(getwd(), "/", subdir, sep="")
+basedir <- str_c(getwd(), "/", subdir)
 create_output_folder(basedir)
 
 # get full address for ptt car
@@ -22,16 +23,17 @@ href_link <- get_page(ppt_car_home, get_type_href)
 # iterator the href_link list
 for (href in href_link[, 1]) {
   
-  ppt_url <- paste(ppt_site, href, sep="")
+  ppt_url <- str_c(ppt_site, href)
   
   # fetch main content
   mc <- get_page(ppt_url, get_type_m_content)
   
   # path concatenation
-  full_path <- paste(basedir, "/",
-                     substr(href, nchar(ppt_site_cat) + 1, nchar(href) - 5),
-                     sep="")
+  full_path <- str_c(basedir,
+                     "/",
+                     str_sub(href, str_length(ppt_site_cat) + 1, str_length(href) - 5))
   
   # write to file (just for debug?)
   str_to_file(mc, full_path)
 }
+ 
