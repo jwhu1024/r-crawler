e3e94a51e465a1d5efb67aeebc2c0658f7fb4738
jwhu1024 <jwhu1024@gmail.com>
2015-11-26 16:55:09 +0800

    sync

--- tm.r
+++ tm.r
@@ -3,36 +3,39 @@ library(tmcn)
 library(Rwordseg)
 library(wordcloud)
 
-d.corpus <- Corpus(DirSource("data"), list(language = NA))                  # read data
-d.corpus <- tm_map(d.corpus, content_transformer(removePunctuation))        # remove punctuation
-d.corpus <- tm_map(d.corpus, content_transformer(removeNumbers))            # remove numbers
-d.corpus <- tm_map(d.corpus, content_transformer(function(word) gsub("[A-Za-z0-9]", "", word)))
+# setwd("/home/lester/Desktop/test")
 
-# words <- readLines("http://wubi.sogou.com/dict/download_txt.php?id=9182") # ptt corpus
-# words <- toTrad(words)
-# insertWords(words)                                                        # tagging
+d.corpus <- VCorpus(DirSource("data"), readerControl = list(language="UTF-8"))
+d.corpus <- tm_map(d.corpus, content_transformer(removePunctuation))
+d.corpus <- tm_map(d.corpus, content_transformer(removeNumbers))
 
-d.corpus <- tm_map(d.corpus, content_transformer(segmentCN), nature = TRUE) # stemming
+# tdm <- TermDocumentMatrix(d.corpus, control = list())
+# inspect(tdm[])
+# stop()
 
-# d.corpus <- tm_map(d.corpus, function(sentence) {                         # feature selection
-#     noun <- lapply(sentence, function(w) {
-#         w[names(w) == "n"]
-#     })
-#     print(noun)
-#     unlist(noun)
-# })
+installDict("ptt.dic", "ptt", dicttype = "text")
 
+d.corpus <- tm_map(d.corpus, content_transformer(segmentCN), nature = TRUE)
 d.corpus <- Corpus(VectorSource(d.corpus))
 
-myStopWords <- c(stopwordsCN(), "編輯", "時間", "標題", "發信", "實業", "作者")
+myStopWords <- c(toTrad(stopwordsCN()), "編輯", "時間", "看板car標題", "發信", "實業", "作者", "發信站", "文章網址")
 d.corpus <- tm_map(d.corpus, content_transformer(removeWords), myStopWords)
 
-tdm = as.matrix(TermDocumentMatrix(d.corpus))
-#print(tdm)
+tdm <- TermDocumentMatrix(d.corpus, control = list())
 
+# debug
+#tmp <- as.matrix(TermDocumentMatrix(d.corpus, control = list()))
+#print(tmp)
+#stop()
+# 前兩篇文章的前10個關鍵字
+#inspect(tdm[1:10, 1:2])
+
+# wordcloud
 m1 <- as.matrix(tdm)
 v <- sort(rowSums(m1), decreasing = TRUE)
 d <- data.frame(word = names(v), freq = v)
-wordcloud(d$word, d$freq, min.freq = 4, random.order = F, ordered.colors = F, 
-          colors = rainbow(length(row.names(m1))))
-#inspect(d.corpus)
+wordcloud(d$word, d$freq, min.freq = 10, random.order = F, random.color = T,
+          ordered.colors = F, colors = rainbow(length(row.names(m1))))
+
+#d.dtm <- DocumentTermMatrix(d.corpus, control = list())
+#inspect(d.dtm[])
